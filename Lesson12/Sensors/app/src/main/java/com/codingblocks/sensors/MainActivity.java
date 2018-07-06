f455d85e7ee5ca8d708e4b20b081ec93919fe286
the-dagger <harshithdwivedi@gmail.com>
2018-07-06 16:49:20 +0530

    add code for gravity sensor
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

---
+++ Lesson12/Sensors/app/src/main/java/com/codingblocks/sensors/MainActivity.java
@@ -0,0 +1,73 @@
+package com.codingblocks.sensors;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    public static final String TAG = "MainActivity";
+    TextView tviewX,tviewY,tviewZ;
+    LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tviewX = findViewById(R.id.gravityInX);
+        tviewY = findViewById(R.id.gravityInY);
+        tviewZ = findViewById(R.id.gravityInZ);
+        linearLayout = findViewById(R.id.lLayout);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        Sensor gravityDefault = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+
+//        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+//        Log.e(TAG, "onCreate: Number of sensors " + sensors.size());
+
+//        for (Sensor s : sensors){
+//            Log.e(TAG, "onCreate: -------------------" );
+//            Log.e(TAG, "onCreate: " + s.toString() );
+//            Log.e(TAG, "onCreate: -------------------" );
+//        }
+
+        //Registers the sensor for updates
+        sensorManager.registerListener(this,gravityDefault,SensorManager.SENSOR_DELAY_UI);
+//        sensorManager.registerListener(this, orientationSensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+//        if (event.sensor.getName().equals("Orientation")){
+//            //Handle the orientation changes
+//        }else if (event.sensor.getName().equals("Gravity")){
+//            //Handle the gravity changes
+//        }
+        float[] valueArray = event.values;
+
+        tviewX.setText(""+ valueArray[0]);
+        tviewY.setText(""+ valueArray[1]);
+        tviewZ.setText(""+ valueArray[2]);
+
+        //Convert the values to colors
+        Color color;
+
+        linearLayout.setBackgroundColor(color);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //This is called whenever the accuracy of the sensor changes
+    }
+}
