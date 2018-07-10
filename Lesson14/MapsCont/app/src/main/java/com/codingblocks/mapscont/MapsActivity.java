cc7d9f885ffd294b2af0c5412d93f5ddd4758694
the-dagger <harshithdwivedi@gmail.com>
2018-07-10 15:30:32 +0530

    add code for drawing shapes on the map
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

---
+++ Lesson14/MapsCont/app/src/main/java/com/codingblocks/mapscont/MapsActivity.java
@@ -0,0 +1,118 @@
+package com.codingblocks.mapscont;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
+
+    //    //This is Hungarian notation, don't use it
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        //Loads the map asynchronously
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+
+        MarkerOptions markerOptions = new MarkerOptions()
+                .draggable(true)
+                .position(sydney)
+                .title("Hello from Sydney!");
+
+        LatLng delhi = new LatLng(28.35, 77.62);
+
+        LatLng antarctica = new LatLng(82.86, 135);
+
+
+        //Create a polyline options, i.e. an open polygon
+        PolylineOptions polylineOptions = new PolylineOptions()
+                .add(sydney,delhi,antarctica);
+
+        //Create a circle option, used to draw circle on the map
+        CircleOptions circleOptions = new CircleOptions()
+                .center(sydney)
+                .radius(1000);
+
+        googleMap.addCircle(circleOptions);
+
+        //Create a polygonOptions, used to draw closed shaped on the map
+        PolygonOptions polygonOptions = new PolygonOptions()
+                .add(sydney, delhi, antarctica);
+
+//        polygonOptions.fillColor(R.color.colorAccent);
+
+        googleMap.addPolygon(polygonOptions);
+
+        googleMap.setOnMarkerDragListener(this);
+
+        googleMap.addMarker(markerOptions);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10.0f));
+//        googleMap.animateCamera(CameraUpdateFactory.newLatLng(sydney));
+//        googleMap.animateCamera(CameraUpdateFactory.zoomIn());
+    }
+
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        //Called when the dragging starts
+
+        LatLng newLatlng = marker.getPosition();
+        //If you want the name of place from the lat and long,
+        //use the google Places API
+        MarkerOptions newMarkerOptions = new MarkerOptions()
+                .position(newLatlng)
+                .title(marker.getId())
+                .draggable(true);
+        mMap.addMarker(newMarkerOptions);
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        //Called when the dragging is being done
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+        //Called when the dragging ends
+        LatLng endLatLng = marker.getPosition();
+        mMap.animateCamera(CameraUpdateFactory
+                .newLatLngZoom(endLatLng, 10.0f));
+    }
+}
